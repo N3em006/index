085cde74b811efe135d4ce80888effd9950597cb
N3em006 <108017426+N3em006@users.noreply.github.com>
2022-11-19 18:11:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let a = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="fd"><a href="" id="a1"><img src="ph1js.png" alt="Error" width="50"></a>
+</div>
+</body>
+`;
+
+function total() {
+    document.write(a)
+}
